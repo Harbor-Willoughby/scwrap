2c58a499bffac9e2cf7b4f9af0eb22ce1680870c
ruucm-working <ruucm.a@gmail.com>
2017-11-18 07:28:19 +0900

    complete basic main_01 layout using Bulma hero layout

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,8 +9,17 @@ export default class Main extends React.Component {
           <span className="scwrap-logo">scwrap</span>
           <span className="scwrap-profile">profile</span>
         </nav>
-        <div className="container main-content">
-          Scwrap 에서 여행을 SCRAP 하세요 ! 
+        <div className="main-content hero is-fullheight">
+          <div className="hero-body">
+            <div className="container">
+              <h1>Scwrap 에서 여행을 SCRAP 하세요 !</h1>
+              <div className="new-trip">
+                <input />
+                <input />
+                <button>NEXT</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
