2418860f552de5e7cd7b0a1ffa30bdb190c333aa
limyongjoo <yjlim@sentience.rocks>
2017-11-29 19:36:04 +0900

    delete useless import line

--- src/stories/trip/trip-detail.js
+++ src/stories/trip/trip-detail.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import firebase from '../../firebase';
-import map from 'lodash/map';
 import { objectValue } from '../../util/objectUtill';
 import {
   connect,
