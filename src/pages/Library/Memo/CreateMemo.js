76973e71edb69a55369435ff63d8cd4800133a01
limyongjoo <yjlim@sentience.rocks>
2017-11-29 14:42:33 +0900

    old creatememo

--- src/pages/Library/Memo/CreateMemo.js
+++ src/pages/Library/Memo/CreateMemo.js
@@ -20,17 +20,23 @@ class CreateMemo extends Component {
   handleClick() {
     console.log("this.state", this.state.memo_text);
     console.log('uid',this.props.uid );
-    firebase.database().ref('/users/' + this.props.uid + '/trips').on('value', snapshot => {
-      const trip_key_value = snapshot.val();
-      console.log("value", JSON.stringify(trip_key_value));
-      const trip_key = JSON.stringify(trip_key_value).split('"')[1];
-      console.log("key", trip_key);
-      firebase.database().ref('/memos').push({
-        tid: trip_key,
-        text: this.state.memo_text,
+    // firebase.database().ref('/users/' + this.props.uid + '/trips').on('value', snapshot => {
+    //   const trip_key_value = snapshot.val();
+    //   console.log("value", JSON.stringify(trip_key_value));
+    //   const trip_key = JSON.stringify(trip_key_value).split('"')[1];
+    //   console.log("key", trip_key);
+    //   const new_memo = firebase.database().ref('/memos').push();
+        // tid: trip_key,
+        // text: this.state.memo_text,
         // uid: this.props.uid
-      })
-    });
+      // const new_memo_key = new_memo.key;
+      // console.log("key", new_memo_key);
+    // });
+    // firebase.database().ref
+    // console.log("this.state", this.state.memo_text);
+    // console.log('uid',this.props.uid );
+    const trip_key = firebase.database().ref('/users/' + this.props.uid + '/trips').on('value');
+    console.log("trip_key", trip_key);
   };
 
   render() {
