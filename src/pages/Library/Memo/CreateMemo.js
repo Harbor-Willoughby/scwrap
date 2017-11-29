e97d1f96cddaeef9c2dab795acadf7e93f100857
limyongjoo <yjlim@sentience.rocks>
2017-11-29 19:34:15 +0900

    divide link_url & memo_text

--- src/pages/Library/Memo/CreateMemo.js
+++ src/pages/Library/Memo/CreateMemo.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import firebase from '../../../firebase';
 import * as ReactDOM from "react-dom";
+import {isEmpty} from "lodash";
 
 
 class CreateMemo extends Component {
@@ -8,30 +9,58 @@ class CreateMemo extends Component {
     super(props);
     this.state = {
       memo_text: "",
+      link_url: "",
+
     }
   }
 
   handleChange(e) {
-    console.log("memo",e.target.value);
-    this.setState({memo_text: e.target.value})
+    const input_text = e.target.value;
+    const reg = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+    console.log('if', reg.test(input_text));
+    if (reg.test(input_text)) {
+      this.setState(
+        {
+          memo_text: "",
+          link_url: input_text
+        }
+      )
+    } else {
+      this.setState(
+        {
+          memo_text: input_text,
+          link_url: ""
+        }
+      )
+    }
   }
 
   handleClick() {
     console.log("memo_text", this.state.memo_text);
+    console.log("link_url", this.state.link_url);
     console.log('trip_key', this.props);
     const trip_key = this.props.tripKey;
-    const memo_data = {
-      text: this.state.memo_text,
-      trip: trip_key
-    };
-    firebase.database().ref('/trips/' + trip_key + '/memos').push(true).then((snapshot) => {
-      const memo_id = snapshot.key;
-      firebase.database().ref('/memos/' + memo_id).set(memo_data).catch((error) => {
-        firebase.database().ref('/trips/' + trip_key + '/memos').remove();
-        console.log('save memo error', error);
+    if (isEmpty(this.state.memo_text)) {
+      /*
+      todo: when input is link...
+      */
+      return console.log('memo empty, todo when link url input');
+    } else if (isEmpty(this.state.link_url)) {
+      const memo_data = {
+        text: this.state.memo_text,
+        trip: trip_key
+      };
+      firebase.database().ref('/trips/' + trip_key + '/memos').push(true).then((snapshot) => {
+        const memo_id = snapshot.key;
+        firebase.database().ref('/memos/' + memo_id).set(memo_data).catch((error) => {
+          firebase.database().ref('/trips/' + trip_key + '/memos').remove();
+          console.log('save memo error', error);
+        });
       });
-    });
-    ReactDOM.findDOMNode(this.refs.memo_text_input).value = '';
+      ReactDOM.findDOMNode(this.refs.memo_text_input).value = '';
+    } else {
+      return console.log('memo or link is required');
+    }
   }
 
 
